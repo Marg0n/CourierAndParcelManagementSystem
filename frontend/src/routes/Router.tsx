b7c40bd3e4133eb386d36192884e2ec4baeaf414
MarGoN <monondcosta@gmail.com>
2025-07-25 23:15:08 +0600

    Add: Added DashboardLayout.

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,20 +2,38 @@ import { Login } from "@/pages/auth/Login";
 import { Registration } from "@/pages/auth/Registration";
 import LandingPage from "@/pages/Landing/LandingPage";
 import { createBrowserRouter } from "react-router-dom";
+import AgentDashboard from "@/pages/dashboard/agent/AgentDashboard";
+import AdminDashboard from "@/pages/dashboard/admin/AdminDashboard";
+import CustomerDashboard from "@/pages/dashboard/customer/CustomerDashboard";
+
 
 const Router = createBrowserRouter([
-    {
-        path: '/',
-        element: <LandingPage />
-    },
-    {
-        path: 'login',
-        element: <Login/>
-    },
-    {
-        path: 'registration',
-        element: <Registration/>
-    },
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "registration",
+    element: <Registration />,
+  },
+
+  //* Dashboard routes
+  {
+    path: "/dashboard/admin",
+    element: <AdminDashboard />,
+  },
+  {
+    path: "/dashboard/customer",
+    element: <CustomerDashboard />,
+  },
+  {
+    path: "/dashboard/agent",
+    element: <AgentDashboard />,
+  },
 ]);
 
 export default Router;
