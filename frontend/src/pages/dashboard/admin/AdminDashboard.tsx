0ca26f4d4a60d1c2739e56eed41d2057942363bf
MarGoN <monondcosta@gmail.com>
2025-07-25 23:16:16 +0600

    Add: Added AdminDashboard.

--- frontend/src/pages/dashboard/admin/AdminDashboard.tsx
+++ frontend/src/pages/dashboard/admin/AdminDashboard.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { DashboardLayout } from "@/components/layouts/DashboardLayout";
+
 
 const AdminDashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <DashboardLayout role="Admin">
+      <h1 className="text-2xl font-bold text-gray-800">Admin Dashboard</h1>
+      <p className="text-gray-600 mt-2">
+        Manage users, parcels, and system analytics.
+      </p>
+    </DashboardLayout>
+  );
 };
 
 export default AdminDashboard;
