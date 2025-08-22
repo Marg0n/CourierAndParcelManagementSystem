d8e22b3b886cbaf43f2ba2dd37b6d8640181c089
MarGoN <monondcosta@gmail.com>
2025-08-22 23:00:13 +0600

    Add: Added error page.

---
+++ frontend/src/pages/shared/errors/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Oops! Page not found.</h1>
+      <p>Sorry, the page you're looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
