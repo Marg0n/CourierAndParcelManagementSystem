7006bf936e653e45b3e0d05389de7be07797cf20
Noc1 PC MargoN <monondcosta@gmail.com>
2025-08-31 10:50:44 +0600

    Modify: Modified Error Page.

--- frontend/src/pages/shared/errors/ErrorPage.tsx
+++ frontend/src/pages/shared/errors/ErrorPage.tsx
@@ -1,24 +1,27 @@
-
-import { useLocation, useNavigate } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-
+  
   //* Navigation
   const navigate = useNavigate();
-  const location = useLocation();
-  const whereTo = location?.state;
-  const fromWhere = location?.pathname;
-
-  console.log(fromWhere,whereTo)
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-100 to-sky-200 flex flex-col justify-center items-center text-center px-4">
-      <h1 className="text-5xl font-bold text-sky-800 mb-4">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-600 mb-6">Oops! The page you're looking for doesn't exist.</p>
-      <Button size="lg" onClick={() => navigate("/")}>
-        Go Back to Home
-      </Button>
+      <h1 className="text-5xl font-bold text-sky-800 mb-4">
+        404 - Page Not Found
+      </h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <div className="flex gap-4">
+        <Button size="lg" onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+        <Button size="lg" onClick={() => navigate("/")}>
+          Go Back to Home
+        </Button>
+      </div>
     </div>
   );
 };
