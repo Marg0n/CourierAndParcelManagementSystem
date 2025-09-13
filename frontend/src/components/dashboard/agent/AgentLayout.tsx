7d3984408886d139319ad180ccf6e69ed128abc3
MarGoN <monondcosta@gmail.com>
2025-09-13 21:33:06 +0600

    Add: Added ScrollToTop in every layout.

--- frontend/src/components/dashboard/agent/AgentLayout.tsx
+++ frontend/src/components/dashboard/agent/AgentLayout.tsx
@@ -1,10 +1,12 @@
 
 import ErrorBoundaryWrapper from '@/pages/shared/errors/ErrorBoundary';
+import ScrollToTop from '@/utils/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 const AgentLayout = () => {
     return (
         <ErrorBoundaryWrapper>
+            <ScrollToTop />
             <Outlet/>
         </ErrorBoundaryWrapper>
     );
--- frontend/src/components/dashboard/customer/Customerlayout.tsx
+++ frontend/src/components/dashboard/customer/Customerlayout.tsx
@@ -1,10 +1,12 @@
 
 import ErrorBoundaryWrapper from '@/pages/shared/errors/ErrorBoundary';
+import ScrollToTop from '@/utils/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 const Customerlayout = () => {
     return (
         <ErrorBoundaryWrapper>
+            <ScrollToTop />
             <Outlet/>
         </ErrorBoundaryWrapper>
     );
