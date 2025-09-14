83e62d37b89d2023768294e5fdbc3b208e285e12
MarGoN <monondcosta@gmail.com>
2025-09-14 22:00:00 +0600

    Add: Added ProfileHeader to shared pages & types.ts to utils.

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,28 @@
+
+
+//* User type definition
+export interface TUser {
+    data: {
+      _id: string;
+      name: string;
+      email: string;
+      role: "admin" | "customer";
+      status: "active" | "inactive";
+      needsPasswordChange: boolean;
+      passwordChangedAt?: Date;
+      createdAt?: Date;
+      updatedAt?: Date;
+      image?: string;
+      address?: string;
+      phone?: string;
+      bloodGroup?: string;
+      emergencyContact?: string;
+      gender?: "male" | "female";
+      dateOfBirth?: Date;
+      country?: string;
+      city?: string;
+      state?: string;
+      zipCode?: string;
+      isDeleted?: boolean;
+    };
+  }
