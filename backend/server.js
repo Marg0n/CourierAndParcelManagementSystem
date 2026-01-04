a5f844f4c8667480994e6034a40a17e9480a4937
Noc1 PC MargoN <monondcosta@gmail.com>
2026-01-04 18:56:12 +0600

    Modify: Modified mongo and server.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,25 @@ import app from "./app.js";
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+(async () => {
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})();
+
+/** Notes
+ * This is called an Immediately Invoked Async Function Expression (usually shortened to async IIFE).
+ * Another way: 
+ * async function startServer() {
+      await connectDB();
+      app.listen(port);
+    }
+
+    startServer();
+ */
