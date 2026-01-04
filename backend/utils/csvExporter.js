88d8446ae4f8f146e717fa928ea49d5991d6079c
Noc1 PC MargoN <monondcosta@gmail.com>
2026-01-04 21:10:32 +0600

    Modify: Modified utils/pdfExporter & csvxporter.js.

--- backend/utils/csvExporter.js
+++ backend/utils/csvExporter.js
@@ -5,3 +5,26 @@ export function exportParcelsToCSV(parcels, res) {
   const csv = parser.parse(parcels);
   res.attachment("parcels.csv").send(csv);
 }
+
+/*
+//! how to use
+import { exportToCSV } from "../utils/csvExporter.js";
+
+const data = await usersCollection.find().toArray();
+const file = exportToCSV(data, "users.csv");
+res.download(file);
+
+*/
+
+/*
+import { Parser } from "json2csv";
+import fs from "fs";
+
+export const exportToCSV = (data, fileName) => {
+  const parser = new Parser();
+  const csv = parser.parse(data);
+
+  fs.writeFileSync(fileName, csv, "utf-8");
+  return fileName;
+};
+*/
