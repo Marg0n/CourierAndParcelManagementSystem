2a8ef9ae3095e22e849e953e3f38b696fceec026
Noc1 PC MargoN <monondcosta@gmail.com>
2026-01-04 18:27:58 +0600

    Modify: Modified veryfyAdmin.

--- backend/middlewares/verifyAdmin.js
+++ backend/middlewares/verifyAdmin.js
@@ -1,9 +1,28 @@
 import { usersCollection } from "../db/mongo.js";
 
-export default async function verifyAdmin(req, res, next) {
-  const user = await usersCollection.findOne({ email: req.decoded.email });
-  if (user?.role !== "Admin") {
-    return res.status(403).send({ message: "Admin only" });
+// export default async function verifyAdmin(req, res, next) {
+//   const user = await usersCollection.findOne({ email: req.decoded.email });
+//   if (user?.role !== "Admin") {
+//     return res.status(403).send({ message: "Admin only" });
+//   }
+//   next();
+// }
+//* ==================================
+//* Admin verify middleware
+//* ==================================
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  // console.log('from verify admin -->', email);
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  const isAdmin = user?.role === "Admin";
+
+  if (!isAdmin) {
+    return res.status(403).send({ message: "Unauthorized!! Admin only!" });
   }
+
   next();
-}
+};
+
+export default verifyAdmin;
